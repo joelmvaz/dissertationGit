96b6ed1c7adf1daa0b71b7bd3ca422a83372d911
Joel Vaz <joelmatosvaz@gmail.com>
2019-11-21 09:14:46 +0000

    Initial backup commit for the dissertation files

---
+++ Dissertation/Data Processing Python/dataParser.py
@@ -0,0 +1,9 @@
+##
+## This Program is meant to Get Sample Data from firebase
+## It's meant to be the building block of the data processing algorythm
+##
+## Author: Joel Matos Vaz - joelmatosvaz@gmail.com
+##
+
+#Import Data Base module
+from firebase import firebase
